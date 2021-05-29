e063158836cce246d7ff6c006c61d1f57dc3f259
Abhilash Raj <abhilash.raj.me@gmail.com>
2021-05-29 09:52:48 +0530

    UPDATE : Source XML File Parser call added

--- controller.py
+++ controller.py
@@ -38,15 +38,36 @@ def main():
     download_path = os.path.join(os.getcwd(),
                                 config.get('download', 'download_path')
                                 )
-
-    log.info('Calling download_xml function')
+    print(download_path)
+    log.info('Calling download function')
     # Calling the download helper function to download the file
-    xml_file = download_xml(url, download_path, 'sourcefile.xml')
+    xml_file = download(url, download_path, 'sourcefile.xml')
 
     # Checking if the file download failed
     if not xml_file:
         print('File Download Fail, Kindly check logs for more details')
         print('Exiting...')
+        return
+    print(xml_file)
+    log.info('Calling parse_source_xml function')
+    # Calling the source xml file parser helper function to download the file
+    file_metadata = parse_source_xml(xml_file)
+    print(file_metadata)
+    # Checking if the required file metadata extraction failed
+    if not file_metadata:
+        print('File Parsing Failed, Kindly check logs for more details')
+        print('Exiting...')
+        return
+
+    # Extracting file name and file download link from file file_metadata
+    filename, file_download_link = file_metadata
+
+    log.info('Calling download function')
+    # Calling the download helper function to download the file
+    xml_zip_file = download(file_download_link, download_path, filename)
+
+
+
 
 
 
